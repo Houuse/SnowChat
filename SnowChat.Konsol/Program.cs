6a4247e4439ec8ff43d89b43f019e1f21b020992
Mohamed <mohamed.almohsen@outlook.com>
2021-05-26 21:31:53 +0200

    First task in the list, TODO, Add some tests, refactor to clean code then do the other tasks

---
+++ SnowChat.Konsol/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using NATS.Client;
+
+namespace SnowChat.Konsol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            const string subject = "*";
+            const string clientName = "ClientName";
+            var cf = new ConnectionFactory();
+            var con = cf.CreateConnection("demo.nats.io:4222");
+            con.Opts.Name = args.Length > 0 ? args[0] : $"Cactus:{Guid.NewGuid().ToString()}";
+            var msgHandler = new EventHandler<MsgHandlerEventArgs>((sender, arguments) =>
+            {
+                Console.WriteLine($"AT {DateTime.Now.ToLongTimeString()}: " +
+                                  $"{arguments.Message.Header[clientName]}: " +
+                                  $"{Encoding.UTF8.GetString(arguments.Message.Data)}");
+            });
+            var sub = con.SubscribeAsync(subject,msgHandler);
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input)) continue;
+                if (input == "quit") break;
+                    Msg msg = new Msg()
+                {
+                    Subject = subject,
+                    Data = Encoding.UTF8.GetBytes(input),
+                };
+                msg.Header[clientName] = con.Opts.Name;
+                con.Publish(msg);
+            }
+            sub.Unsubscribe();
+            con.Close();
+        }
+    }
+}
